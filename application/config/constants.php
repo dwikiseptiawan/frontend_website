4c39d1d87c450979e9c1de72403e68d9adb3dec4
Marta Sasmita 20312051 <marta_sasmita@teknokrat.ac.id>
2022-12-24 08:20:54 +0700

    update constant url

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,5 +85,10 @@ defined('EXIT__AUTO_MIN')      or define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      or define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 // custom constant (Konstanta)
-define("API", "http://localhost/PWBS-GAB2/Server");
-define("APIMAHASISWA", API . "/index.php/Mahasiswa");
+define("API", "http://localhost/backend_website");
+define("APIIMAGE", API . "/index.php/Mahasiswa");
+define("APINUMBER", API . "/index.php/Mahasiswa");
+define("APIUSER", API . "/index.php/Mahasiswa");
+define("APIVALIDATION", API . "/index.php/Validation");
+define("URL_DASHBOARD", "http://localhost/frontend_website/ext/tampilan_admin/");
+define("URL_UTAMA", "http://localhost/frontend_website/ext/tampilan_utama/");
