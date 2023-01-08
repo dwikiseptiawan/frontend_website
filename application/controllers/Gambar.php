de346e5a68151377584c84c2b33f0fe169faf7af
Dwiki Septiawan 20312059 <dwiki_septiawan@teknokrat.ac.id>
2023-01-08 22:29:41 +0700

    buat controller gambar

---
+++ application/controllers/Gambar.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Gambar extends CI_Controller
+{
+	public function __construct()
+    {
+        parent::__construct();
+
+        // Load session library
+        $this->load->library('session');
+    }
+
+	public function menu_gambar()
+	{
+		$data['user'] = json_decode($this->client->simple_get(APIUSER));
+		$this->load->view('gambar', $data);
+	}
+
+	public function delete()
+	{
+		$json  = file_get_contents("php://input");
+		$hasil = json_decode($json);
+		$delete = json_decode($this->client->simple_delete(APIUSER, array("id" => $this->input->post("id"))));
+		redirect("pengguna");
+	}
+
+	function tambah()
+	{
+		$data = array(
+			"nama" => $this->input->post("nama"),
+			"username" => $this->input->post("username"),
+			"password" => $this->input->post("password"),
+			"type" => "1",
+		);
+
+		$save = json_decode($this->client->simple_post(APIUSER, $data));
+		redirect("pengguna");
+	}
+}
