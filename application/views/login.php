7a567769354466c5d6a396cdb7a501cf49eedbd3
Marta Sasmita 20312051 <marta_sasmita@teknokrat.ac.id>
2023-01-17 10:41:16 +0700

    merapihkan tampilan login

--- application/views/login.php
+++ application/views/login.php
@@ -9,15 +9,12 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
   <link href="<?= URL_DASHBOARD ?>assets/img/favicon.png" rel="icon">
   <link href="<?= URL_DASHBOARD ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
   <link href="<?= URL_DASHBOARD ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -25,16 +22,8 @@
   <link href="<?= URL_DASHBOARD ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
   <link href="<?= URL_DASHBOARD ?>assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin - v2.4.1
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
@@ -96,15 +85,6 @@
 
                 </div>
               </div>
-
-              <!-- <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-              <!-- You can delete the links only if you purchased the pro version. -->
-              <!-- Licensing information: https://bootstrapmade.com/license/ -->
-              <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-              <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
-              <!-- </div> -->
-
             </div>
           </div>
         </div>
@@ -125,8 +105,6 @@
   <script src="<?= URL_DASHBOARD ?>assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="<?= URL_DASHBOARD ?>assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="<?= URL_DASHBOARD ?>assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
   <script src="<?= URL_DASHBOARD ?>assets/js/main.js"></script>
 
 </body>
