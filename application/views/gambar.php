2a4fcccb50f3e7f8744db73d90aa5b4a471d2984
Marta Sasmita 20312051 <marta_sasmita@teknokrat.ac.id>
2023-01-17 10:41:03 +0700

    merapihkan tampilan gambar

--- application/views/gambar.php
+++ application/views/gambar.php
@@ -9,15 +9,12 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
   <link href="<?= URL_DASHBOARD ?>assets/img/favicon.png" rel="icon">
   <link href="<?= URL_DASHBOARD ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
   <link href="<?= URL_DASHBOARD ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -25,16 +22,8 @@
   <link href="<?= URL_DASHBOARD ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?= URL_DASHBOARD ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
   <link href="<?= URL_DASHBOARD ?>assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: NiceAdmin - v2.4.1
-  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
